66c6715b99b7d04f1ca22b5a26f0d3ed9d7b8b8c
AlbinAbsint <albin@cedercentral.com>
2024-12-03 18:20:56 +0100

    homework_2
    -Added the avg_ppsqm for the whole Ekhagen area

---
+++ utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def fill_missing_values(row, year_cols, increase_percentage):
+    last_valid_value = None
+    second_last_valid_value = None
+    for i in range(len(year_cols)):
+        if pd.isna(row[year_cols[i]]):
+            if last_valid_value is None:
+                row[year_cols[i]] = 0
+            else:
+                if second_last_valid_value is None:
+                    row[year_cols[i]] = last_valid_value * (1 + increase_percentage)
+                    second_last_valid_value = last_valid_value
+                    last_valid_value = row[year_cols[i]]
+                else:
+                    diff = last_valid_value - second_last_valid_value
+                    row[year_cols[i]] = last_valid_value + diff
+                    second_last_valid_value = last_valid_value
+                    last_valid_value = row[year_cols[i]]
+        else:
+            second_last_valid_value = last_valid_value
+            last_valid_value = row[year_cols[i]]
+    return row
